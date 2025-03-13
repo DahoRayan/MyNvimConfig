e8a41083459d9f8676812b56fb6216baff36a9e6
Daho Bachir Rayan <dahobachirrayan@gmail.com>
2025-03-13 15:51:33 +0100

    some options and neccessary plugins, not yet an lsp

---
+++ lua/config/options.lua
@@ -0,0 +1,26 @@
+vim.opt.number = true
+vim.opt.relativenumber = true 
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
+vim.opt.tabstop = 4 -- How many spaces are shown per Tab
+vim.opt.softtabstop = 4 -- How many spaces are applied when pressing Tab
+vim.opt.breakindent = true
+
+vim.opt.smarttab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true -- Keep identation from previous line
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.splitright = true
+
+vim.opt.scrolloff = 10
